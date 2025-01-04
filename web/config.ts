470a46f31dbc80cc08721d00b9fd314545e515c6
Jefferson <ifrit68@hotmail.com>
2025-01-04 11:23:05 -0500

    FEATURE[koyeb]

--- web/config.ts
+++ web/config.ts
@@ -2,3 +2,5 @@ export const deployment =
   process.env.NODE_ENV === "development"
     ? { path: process.env.BACKEND_DEV_PATH }
     : { path: process.env.BACKEND_PROD_PATH };
+
+export const test = "test";
